2a90a05c37cb8f44ae20dac4a8c4cdc417eb8295
Foysal Hossain <foysalx4@gmail.com>
2024-05-11 15:18:19 +0600

    blog-section

--- src/components/blogs/Blog.tsx
+++ src/components/blogs/Blog.tsx
@@ -1,6 +1,25 @@
+import { blogData } from "./BlogData";
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div id="Blog" className="py-10">
+      <div className="md:p-14 p-6">
+        <h1 className="text-4xl font-semibold">Blog</h1>
+
+        <div className="mt-8 py-2">
+          {blogData.map((blog, i) => (
+            <div key={i} className="collapse collapse-arrow bg-base-200 my-4">
+              <input type="radio" name="my-accordion-2" />
+              <div className="collapse-title text-xl font-medium">
+                {blog.name}
+              </div>
+              <div className="collapse-content">
+                <a href="#">See More</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/components/blogs/BlogData.tsx
@@ -0,0 +1,14 @@
+export const blogData = [
+    {
+        name: "10 Things Every Programmers and Software Developer Should Know"
+    },
+    {
+        name: "15 JavaScript concepts that every JavaScript Programmer must know."
+    },
+    {
+        name: "8 Critical Soft Skills Every Developer Should Have in 2024"
+    },
+    {
+        name: "The Power of Next.js: Building the Future of Web Development"
+    },
+]
