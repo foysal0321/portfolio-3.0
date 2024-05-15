cec04072642b961996be4a81e2b8f6a4f6687779
Foysal Hossain <foysalx4@gmail.com>
2024-05-15 17:12:09 +0600

    hide-from

--- src/app/(DashbordLayout)/dashboard/page.tsx
+++ src/app/(DashbordLayout)/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function BlogPage() {
       <div className="max-w-[1100px] mx-auto py-8">
       <h2 className="text-3xl py-2 font-bold">Create a new blog</h2> 
       </div>
-      <BlogForm />
+      {/* <BlogForm /> */}
     </div>
   )
 }
